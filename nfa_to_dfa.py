edf404867190ccdcb62b67748e2e7385b489dde9
Alex Tenita <tenitaalex@gmail.com>
2022-04-04 00:02:47 +0300

    document the functions till get_States+after+lambda+transitions

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -1,6 +1,5 @@
-from typing import Tuple, List, Dict
+from typing import Tuple, List, TextIO
 import graphviz
-from collections import defaultdict
 
 LAMBDA = 'lambda'
 
@@ -20,132 +19,98 @@ LAMBDA = 'lambda'
 class NFA:
 
     """
-    Functie folosita pentru citirea starilor, tranzitiilor si starilor finale ale AFN
+    Functie folosita pentru citirea datelor asociate AFN-ului, din fisierul de input:
+        starile
+        starea initiala
+        starile finale
+        alfabetul
+        tranzitiile
     """
-    # def read_input(self) -> Tuple[int, List, str, set, Dict,list]:
     def read_input(self) -> Tuple[int, List, str, List, List, List]:
         lines = self.input_file.readlines()
-        # number_of_states = int(lines[0])
 
         states = set(lines[0].split())
-        number_of_states = len(states)
 
         start_state = lines[1].split()[0]
 
         final_states = set(lines[2].split())
 
         alphabet = set(lines[3].split())
-
         alphabet.add(LAMBDA)
 
         delta = []
 
         for line in lines[4:]:
             line = line.split()
+
             curr_state = line[0]
-
             next_state = line[2]
-
             symbol = line[1]
 
-            if line[1] == LAMBDA:
-                symbol = LAMBDA
-                alphabet.add(symbol)
-            else:			
-                symbol = line[1]
-                alphabet.add(symbol)
-
-            # next_states = []
-            # for state in line[2:]:
-            #     next_states.append(state)
-
-            # delta[(curr_state, symbol)] = next_states
             delta.append([curr_state, symbol, next_state])
 
-        return (number_of_states, list(states), start_state, list(final_states), delta, list(alphabet))
+        return (list(states), start_state, list(final_states), delta, list(alphabet))
 
 
-    def __init__(self, input_file, output_file) -> None:
-        # self.no_state = no_state
-        # self.states = states
-        # self.no_alphabet = no_alphabet
-        # self.alphabet = alphabet
-        
-        # Adding epsilon alphabet to the list
-        # and incrementing the alphabet count
-        # self.alphabet.append('e')
-        # self.no_alphabet += 1
-
-        # ==
+    def __init__(self, input_file: TextIO, output_file:TextIO) -> None:
         self.input_file = input_file
         self.output_file = output_file
-        (self.number_of_states,
-        self.states,
+
+        (self.states,
         self.start_state,
         self.final_states,
         self.delta,
         self.alphabet) = self.read_input()
-        self.alphabet_length = len(self.alphabet)
-
-        # self.delta_length = len(self.delta.keys())
-        self.delta_length = len(self.delta)
-
-        print(f" states: {self.states}, final: {self.final_states}, delta: {self.delta}, alphabet: {self.alphabet}, delta_lenght: {self.delta_length} ")
-        # == 
-        
-        print(f"range of self.delta_length: ", range(self.delta_length))
-
-        # self.start = start
 
         self.graph = graphviz.Digraph()
 
-        # Dictionaries to get index of states or alphabet
-        self.states_dict = dict()
-        for i in range(self.number_of_states):
-            self.states_dict[self.states[i]] = i
-        self.alphabet_dict = dict()
-        for i in range(self.alphabet_length):
-            self.alphabet_dict[self.alphabet[i]] = i
+        # popularea unor dictionare pentru a avea o mapare [valoare]: index
+        self.states_to_index_dict = dict()
+        for state_index in range(len(self.states)):
+            self.states_to_index_dict[self.states[state_index]] = state_index
+
+        self.alphabet_to_index_dict = dict()
+        for operator_index in range(len(self.alphabet)):
+            self.alphabet_to_index_dict[self.alphabet[operator_index]] = operator_index
             
-        # delta table is of the form
-        # [From State + Alphabet pair] -> [Set of To States]
+        # delta_table este de forma [Index stare initiala + index operator din alfabet]: [Starile noi]
         self.delta_table = dict()
-        for i in range(self.number_of_states):
-            for j in range(self.alphabet_length):
-                self.delta_table[str(i)+str(j)] = []
-        for i in range(self.delta_length):
-            # print(f"\n\nself.delta[{self.states[i]}][2]")
-            # print(f"self.delta:", self.delta)
-            # print(f".append(self.states_dict[{self.delta[self.states[i]][2]}])")
-            print(f"self.delta_table[{str(self.states_dict[self.delta[i][0]])+str(self.alphabet_dict[self.delta[i][1]])}]")
-            self.delta_table[str(self.states_dict[self.delta[i][0]])
-                                + str(self.alphabet_dict[
-                                    self.delta[i][1]])].append(
-                                        self.states_dict[self.delta[i][2]])
+        for state_index in range(len(self.states)):
+            for alphabet_index in range(len(self.alphabet)):
+                # initializarea tuturor cheilor stare+operator
+                self.delta_table[str(state_index)+','+str(alphabet_index)] = []
+
+        for i in range(len(self.delta)):
+            # popularea
+            self.delta_table[
+                str(self.states_to_index_dict[self.delta[i][0]])+ ','
+                str(self.alphabet_to_index_dict[self.delta[i][1]])
+            ].append(
+                self.states_to_index_dict[self.delta[i][2]]
+                )
 
 
-
-    def get_lambda_closures(self, state):
-        # Method to get Epsilon Closure of a state of NFA
-        # Make a dictionary to track if the state has been visited before
-        # And a array that will act as a stack to get the state to visit next
+    """
+    Functie folosita pentru a returna starile dupa lambda tranzitii asociate unei stari trimise ca parametru
+    """
+    def get_states_after_lambda_transitions(self, state):
         closure = dict()
-        closure[self.states_dict[state]] = 0
-        closure_stack = [self.states_dict[state]]
+        closure[self.states_to_index_dict[state]] = 0
+        closure_queue = [self.states_to_index_dict[state]]
 
         # While stack is not empty the loop will run
-        while (len(closure_stack) > 0):
+        while (len(closure_queue) > 0):
         
             # Get the top of stack that will be evaluated now
-            cur = closure_stack.pop(0)
+            cur = closure_queue.pop(0)
             
             # For the epsilon transition of that state,
             # if not present in closure array then add to dict and push to stack
             for x in self.delta_table[
-                    str(cur)+str(self.alphabet_dict[LAMBDA])]:
+                    str(cur)+','+str(self.alphabet_to_index_dict[LAMBDA])]:
                 if x not in closure.keys():
                     closure[x] = 0
-                    closure_stack.append(x)
+                    closure_queue.append(x)
             closure[cur] = 1
         return closure.keys()
 
@@ -164,13 +129,10 @@ class NFA:
         # by checking if any of the set is a final state in NFA
         for x in state_list:
             for y in self.final_states:
-                if (x == self.states_dict[y]):
+                if (x == self.states_to_index_dict[y]):
                     return True
         return False
 
-
-
-# =======
 
 # Making an object of Digraph to visualize NFA diagram
 nfa = NFA(open('input.txt'), open('output.txt', 'w'))
@@ -181,7 +143,7 @@ dfa_vGraf = graphviz.Digraph()
 # Finding epsilon closure beforehand so to not recalculate each time
 epsilon_closure = dict()
 for x in nfa.states:
-	epsilon_closure[x] = list(nfa.get_lambda_closures(x))
+	epsilon_closure[x] = list(nfa.get_states_after_lambda_transitions(x))
 
 # First state of DFA will be epsilon closure of start state of NFA
 # This list will act as stack to maintain till when to evaluate the states
@@ -216,7 +178,7 @@ while (len(dfa_stack) > 0):
 		for x in cur_state:
 			# Performing Union update and adding all the new states in set
 			from_closure.update(
-				set(nfa.delta_table[str(x)+str(al)]))
+				set(nfa.delta_table[str(x)+','+str(al)]))
 
 		# Check if epsilon closure of the new set is not empty
 		if (len(from_closure) > 0):

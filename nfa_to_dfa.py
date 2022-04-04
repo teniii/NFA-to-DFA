a02712bc2bdfca50a91fd6ae327fc87b7fc744cf
Alex Tenita <tenitaalex@gmail.com>
2022-04-04 10:38:18 +0300

    Finish to document the nfa_to_dfa file

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -2,19 +2,28 @@ from typing import Tuple, List, TextIO
 import graphviz
 
 LAMBDA = 'lambda'
+GRAPH_REPR = graphviz.Digraph()
+
 
 """
     Clasa pentru AFN
 
     Campuri:
         input_file
-        output_file
-        alphabet - symbols for transitions (type = set)
-        number_of_states
-        states (type = list[int])
-        final_states (type = list[int])
-        delta - multimea tranzitiilor dintr-o stare X in alta stare Y prin tranzitia T
+        output_file_name
+        states (type = list[str])
+        start_state (type = str)
+        final_states (type = list[str])
+        delta - multimea tranzitiilor dintr-o stare X in alta stare Y prin operatorul T
                 in format [X, T, Y] (type = list[list[string]])
+        alphabet - symbols for transitions (type = list)
+        
+        == utilitare ==
+
+        states_to_index_dict (type = dict[str:int])
+        alphabet_to_index_dict (type = dict[str:int])
+        delta_table (type = dict['initial_state_id'+'operator' : list['new_states']])
+
 """
 class NFA:
 
@@ -35,8 +44,9 @@ class NFA:
 
         final_states = set(lines[2].split())
 
-        alphabet = set(lines[3].split())
-        alphabet.add(LAMBDA)
+        alphabet = lines[3].split()
+        if(LAMBDA not in alphabet):
+            alphabet.append(LAMBDA)
 
         delta = []
 
@@ -52,9 +62,9 @@ class NFA:
         return (list(states), start_state, list(final_states), delta, list(alphabet))
 
 
-    def __init__(self, input_file: TextIO, output_file:TextIO) -> None:
+    def __init__(self, input_file: TextIO, output_file_name:TextIO) -> None:
         self.input_file = input_file
-        self.output_file = output_file
+        self.output_file_name = output_file_name
 
         (self.states,
         self.start_state,
@@ -62,9 +72,7 @@ class NFA:
         self.delta,
         self.alphabet) = self.read_input()
 
-        self.graph = graphviz.Digraph()
-
-        # popularea unor dictionare pentru a avea o mapare [valoare]: index
+        # Popularea unor dictionare pentru a avea o mapare [valoare]: index
         self.states_to_index_dict = dict()
         for state_index in range(len(self.states)):
             self.states_to_index_dict[self.states[state_index]] = state_index
@@ -74,15 +82,15 @@ class NFA:
             self.alphabet_to_index_dict[self.alphabet[operator_index]] = operator_index
         
 
-        # delta_table este de forma [Index stare initiala + index operator din alfabet]: [Starile noi]
+        # Delta_table este de forma [Index stare initiala + index operator din alfabet]: [Starile noi]
         self.delta_table = dict()
         for state_index in range(len(self.states)):
             for alphabet_index in range(len(self.alphabet)):
-                # initializarea tuturor cheilor stare+operator
+                # Initializarea tuturor cheilor stare+operator
                 self.delta_table[str(state_index)+str(alphabet_index)] = []
 
         for i in range(len(self.delta)):
-            # popularea
+            # Popularea
             self.delta_table[
                 str(self.states_to_index_dict[self.delta[i][0]])+
                 str(self.alphabet_to_index_dict[self.delta[i][1]])
@@ -95,12 +103,12 @@ class NFA:
     Functie folosita pentru a returna lambda inchiderile (starile dupa lambda tranzitii) asociate unei stari trimise ca parametru
     """
     def get_states_after_lambda_transitions(self, state) -> List:
-        # dict care stocheaza toate starile in care se poate ajunge cu lamba tranzitii
-        # initial valorile sunt 0, iar odata ce o stare a fost verificata, este marcata cu 1
+        # Dict care stocheaza toate starile in care se poate ajunge cu lamba tranzitii
+        # Initial valorile sunt 0, iar odata ce o stare a fost verificata, este marcata cu 1
         state_verified_dict = dict()
         state_verified_dict[self.states_to_index_dict[state]] = 0
 
-        # coada cu starile ce mai trebuie verificate
+        # Coada cu starile ce mai trebuie verificate
         queue = [self.states_to_index_dict[state]]
 
         # Cat timp mai exista stari in coada, iteram
@@ -109,14 +117,16 @@ class NFA:
             # Extragem prima stare din coada
             current_state = queue.pop(0)
             
-            # For the epsilon transition of that state,
-            # if not present in closure array then add to dict and push to stack
+            # Iteram prin lambda inchiderile starii curente
             for x in self.delta_table[
             str(current_state)+
             str(self.alphabet_to_index_dict[LAMBDA])]:
+                # Daca starea nu este in dictionar, o adaugam ca neverificata si o adaugam in coada
                 if x not in state_verified_dict.keys():
                     state_verified_dict[x] = 0
                     queue.append(x)
+
+            # La finalizarea iteratiei starea curenta se considera ca fiind verificata
             state_verified_dict[current_state] = 1
         return list(state_verified_dict.keys())
 
@@ -128,7 +138,7 @@ class NFA:
 
         list_for_join = [self.states[state] for state in states]
 
-        return "[" + ",".join(list_for_join) + ']'
+        return "{" + ",".join(list_for_join) + '}'
     
 
     """
@@ -143,77 +153,90 @@ class NFA:
                     return True
         return False
 
+    """
+    Functie pentru a adauga prima stare AFD in reprezentarea visuala sub forma de graf
+    """
+    def add_first_node_to_graph(self, state):
+        # Verifiicare daca primul nod este stare finala in AFD
+        if (self.is_final_DFA_state(state)):
+            GRAPH_REPR.attr('node', shape='doublecircle')
+        else:
+            GRAPH_REPR.attr('node', shape='circle')
+        GRAPH_REPR.node(self.get_DFA_state_name(state))
 
-# Making an object of Digraph to visualize NFA diagram
-nfa = NFA(open('input.txt'), open('output.txt', 'w'))
+        # Adaugam sageata la starea de start
+        GRAPH_REPR.attr('node', shape='none')
+        GRAPH_REPR.node('')
+        GRAPH_REPR.edge('', self.get_DFA_state_name(state))
 
-dfa_vGraf = graphviz.Digraph()
+
+    """
+    Functie pentru conversia la AFD
+    """
+    def convert_to_DFA(self):
+        queue = list()
+
+        # pornim de la lambda inchiderile starii de start
+        queue.append(self.get_states_after_lambda_transitions(self.start_state))
+        
+        # adaugarea primului nod in graf
+        self.add_first_node_to_graph(queue[0])
+
+        # lista cu toate starile AFD
+        dfa_states = list()
+
+        # prima stare este formata chiar din lambda inchiderile starii de start
+        dfa_states.append(queue[0])
+
+        while (len(queue) > 0):
+            # Starea curenta AFD
+            current_DFA_state = queue.pop(0)
+
+            # Iteram prin alfabet pentru a calcula tranzitiile in AFD
+            for op_index in range(len(self.alphabet) - 1):
+
+                initial_NFA_trans = set()
+                for NFA_state in current_DFA_state:
+                    # Adaugam toate tranzitiile starilor AFN din starea AFD curenta
+                    initial_NFA_trans.update(
+                        set(self.delta_table[str(NFA_state)+str(op_index)]))
+
+                # Verificam multimea sa nu fie goala
+                if (len(initial_NFA_trans) > 0):
+                    # Stari AFN dupa lambda tranzitii <=> Stare AFD
+                    NFA_states_after_lambda_trans = set()
+                    for NFA_state in list(initial_NFA_trans):
+                        # Pentru fiecare stare se populeaza multimea starilor dupa lamda tranziti
+                        NFA_states_after_lambda_trans.update(set(self.get_states_after_lambda_transitions(self.states[NFA_state])))
+
+                    # Verificam daca lista de lambda inchideri (starea AFD) nu este deja in dfa_states
+                    # Se poate adauga un else pentru a face un AFD complet
+                    if list(NFA_states_after_lambda_trans) not in dfa_states:
+                        # Adaugam in coada noua stare AFD
+                        queue.append(list(NFA_states_after_lambda_trans))
+                        # Adaugam in lista de stari AFD noua stare generata
+                        dfa_states.append(list(NFA_states_after_lambda_trans))
+
+                # ======= AFISARE =======
+                        # Verificam daca noua stare este finala, pentru afisare
+                        if (self.is_final_DFA_state(list(NFA_states_after_lambda_trans))):
+                            GRAPH_REPR.attr('node', shape='doublecircle')
+                        else:
+                            GRAPH_REPR.attr('node', shape='circle')
+                        GRAPH_REPR.node(self.get_DFA_state_name(list(NFA_states_after_lambda_trans)))
+
+                    # Adaugarea arcului de la starea curenta la starea urmatoare
+                    GRAPH_REPR.edge(self.get_DFA_state_name(current_DFA_state),
+                            self.get_DFA_state_name(list(NFA_states_after_lambda_trans)),
+                            label=self.alphabet[op_index])
+                # ======= |||||| =======
+
+        # Randarea grafului
+        GRAPH_REPR.render(self.output_file_name)
 
 
-# Finding epsilon closure beforehand so to not recalculate each time
-epsilon_closure = dict()
-for x in nfa.states:
-	epsilon_closure[x] = list(nfa.get_states_after_lambda_transitions(x))
+# Creara unei instante a clasei
+nfa = NFA(open('input.txt'), 'conversion_to_DFA')
 
-# First state of DFA will be epsilon closure of start state of NFA
-# This list will act as stack to maintain till when to evaluate the states
-dfa_stack = list()
-dfa_stack.append(epsilon_closure[nfa.start_state])
-
-# Check if start state is the final state in DFA
-if (nfa.is_final_DFA_state(dfa_stack[0])):
-	dfa_vGraf.attr('node', shape='doublecircle')
-else:
-	dfa_vGraf.attr('node', shape='circle')
-dfa_vGraf.node(nfa.get_DFA_state_name(dfa_stack[0]))
-
-# Adding start state arrow to start state in DFA
-dfa_vGraf.attr('node', shape='none')
-dfa_vGraf.node('')
-dfa_vGraf.edge('', nfa.get_DFA_state_name(dfa_stack[0]))
-
-# List to store the states of DFA
-dfa_states = list()
-dfa_states.append(epsilon_closure[nfa.start_state])
-
-# Loop will run till this stack is not empty
-while (len(dfa_stack) > 0):
-	# Getting top of the stack for current evaluation
-	cur_state = dfa_stack.pop(0)
-
-	# Traversing through all the alphabet for evaluating transitions in DFA
-	for al in range((len(nfa.alphabet)) - 1):
-		# Set to see if the epsilon closure of the set is empty or not
-		from_closure = set()
-		for x in cur_state:
-			# Performing Union update and adding all the new states in set
-			from_closure.update(
-				set(nfa.delta_table[str(x)+str(al)]))
-
-		# Check if epsilon closure of the new set is not empty
-		if (len(from_closure) > 0):
-			# Set for the To state set in DFA
-			to_state = set()
-			for x in list(from_closure):
-				to_state.update(set(epsilon_closure[nfa.states[x]]))
-
-			# Check if the to state already exists in DFA and if not then add it
-			if list(to_state) not in dfa_states:
-				dfa_stack.append(list(to_state))
-				dfa_states.append(list(to_state))
-
-				# Check if this set contains final state of NFA
-				# to get if this set will be final state in DFA
-				if (nfa.is_final_DFA_state(list(to_state))):
-					dfa_vGraf.attr('node', shape='doublecircle')
-				else:
-					dfa_vGraf.attr('node', shape='circle')
-				dfa_vGraf.node(nfa.get_DFA_state_name(list(to_state)))
-
-			# Adding edge between from state and to state
-			dfa_vGraf.edge(nfa.get_DFA_state_name(cur_state),
-					nfa.get_DFA_state_name(list(to_state)),
-					label=nfa.alphabet[al])
-
-# Makes a pdf with name dfa.pdf and views the pdf
-dfa_vGraf.render('dfa', view = True)
+# Apelarea functiei de convertire din AFN in AFD
+nfa.convert_to_DFA()
